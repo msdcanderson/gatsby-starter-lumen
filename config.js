09c4fa01c3490f4e6a7c222e467687e5d2f7751f
michaelanderson <michael.anderson@banksearch-consultancy.com>
2019-04-07 22:20:08 +0100

    update config

--- config.js
+++ config.js
@@ -1,38 +1,47 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://lumen.netlify.com",
+  title: "Hollie Marshall",
+  subtitle:
+    "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/"
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About",
+      path: "/pages/about"
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: "People",
+      path: "/pages/people"
+    },
+    {
+      label: "Publications",
+      path: "/pages/publications"
+    },
+    {
+      label: "Research",
+      path: "/pages/research"
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: "Hollie Marshall",
+    photo: "/photo.jpg",
+    bio: "Moo",
     contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
+      email: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "#",
+      vkontakte: "#"
     }
   }
 };
